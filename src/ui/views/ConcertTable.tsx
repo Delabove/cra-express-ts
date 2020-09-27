3d9565a64afff438f182e542785b25db65319403
DeLayne LaBove <delayne.labove@gmail.com>
2020-09-27 11:38:16 -0500

    table setup

---
+++ src/ui/views/ConcertTable.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Concerts = () => {
+  return (
+    <div className='concertList'>
+      <Link to='#'>Concert List</Link>
+    </div>
+  )
+}
+
+export default Concerts
